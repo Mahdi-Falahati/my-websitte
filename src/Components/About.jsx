5279fcf4c9ccb6c1c44566bd73f60b4bcb0d4264
Mahdi-Falahati <falahati01mahdi@gmail.com>
2022-07-13 12:23:41 +0430

    remove ying yung from about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,44 +9,41 @@ const About = () => {
                 <span>About Me</span>
                 <span></span>
             </div>
-            <div className={styles.wrapper}>
-                <div className={styles.yin_yang}></div>
-
-                <div className={styles.wrap}>
-                    <section className={styles.card}>
-                        <main className={styles.card_image}>
-                            <div className={styles.info}>
-                                <p> My name is mahdi falahati I am 21 years old </p>
-                                <p> I am from iran ( gilan ) </p>
-                            </div>
-                            <hr />
-                            <ul className={styles.habbies}>
-                                <h4 className={styles.habbies__title}>My hobbies are....</h4>
-                                <li>listening to music</li>
-                                <li>playing chess</li>
-                                <li>Writing code & Reading the source code</li>
-                            </ul>
-                        </main>
-                        <div className={styles.card_description}>
-                            <div className={styles.RCImg}>
-                                <img src={RC} title="RC" alt="RC" />
-                            </div>
+            <div className={styles.wrap}>
+                <section className={styles.card}>
+                    <main className={styles.card_image}>
+                        <div className={styles.info}>
+                            <p> My name is mahdi falahati I am 21 years old </p>
+                            <p> I am from iran ( gilan ) </p>
                         </div>
-                    </section>
+                        <hr />
+                        <ul className={styles.habbies}>
+                            <h4 className={styles.habbies__title}>My hobbies are....</h4>
+                            <li>listening to music</li>
+                            <li>playing chess</li>
+                            <li>Writing code & Reading the source code</li>
+                        </ul>
+                    </main>
+                    <div className={styles.card_description}>
+                        <div className={styles.RCImg}>
+                            <img src={RC} title="RC" alt="RC" />
+                        </div>
+                    </div>
+                </section>
 
-                    <section className={styles.AboutDescription}>
-                        <p> I am studying computer engineering majoring in software at Gonbad Kavos University </p>
-                        <p> Energetic and I am always positive , I also bring energy to the workplace and my teammates </p>
-                        <p> Highly resilient in pursuing my goals and can consistently learn new knowledge </p>
-                        <footer className={styles.bio__footer}>
-                            <p>My strengths are I am a self-motivated and hard-working person.</p>
-                            <p>My weakness is I easily trust everyone</p>
-                        </footer>
-                    </section>
-                </div>
+                <section className={styles.AboutDescription}>
+                    <p> I am studying computer engineering majoring in software at Gonbad Kavos University </p>
+                    <p> Energetic and I am always positive , I also bring energy to the workplace and my teammates </p>
+                    <p> Highly resilient in pursuing my goals and can consistently learn new knowledge </p>
+                    <footer className={styles.bio__footer}>
+                        <p>My strengths are I am a self-motivated and hard-working person.</p>
+                        <p>My weakness is I easily trust everyone</p>
+                    </footer>
+                </section>
             </div>
         </div>
     );
 };
 
 export default About;
+
